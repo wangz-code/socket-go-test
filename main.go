69563aa3cced2e4d6816c86d34ee65f70eda2068
wangqz <2809755637@qq.com>
2023-03-30 16:04:01 +0800

    init

---
+++ main.go
@@ -0,0 +1,60 @@
+// Package main runs a go-socket.io based websocket server.
+// An Iris compatible clone of: https://github.com/googollee/go-socket.io#example,
+// use of `iris.FromStd` to convert its handler.
+package main
+
+import (
+	"fmt"
+
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/kataras/iris/v12"
+)
+
+var ids []socketio.Conn
+
+func main() {
+	app := iris.New()
+	server := socketio.NewServer(nil)
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		ids = append(ids, s)
+		return nil
+	})
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		s.Emit("reply", msg)
+		fmt.Println("notice:", msg)
+		fmt.Println("ids:", ids)
+		// for i := 0; i < len(ids); i++ {
+		// 	ids[i].Emit("reply", msg)
+		// }
+	})
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+	server.OnError("/", func(s socketio.Conn, e error) {
+		fmt.Println("meet error:", e)
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		fmt.Println("closed", reason)
+	})
+	go server.Serve()
+	defer server.Close()
+
+	app.HandleMany("GET POST", "/socket.io/{any:path}", iris.FromStd(server))
+	app.HandleDir("/", iris.Dir("./asset"))
+
+	app.Listen(":8000", iris.WithoutPathCorrection)
+}
+
+/*
+If you want to enable CORS in your websocket handler,
+please follow this post: https://github.com/googollee/go-socket.io/issues/242
+*/
